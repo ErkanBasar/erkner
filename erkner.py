b75177d88099a4709fc0549ad61faeb8dddfc56c
Mustafa Erkan Başar <mustafaerkanbasar@gmail.com>
2015-11-29 01:45:37 +0100

    training(manual) tags added

--- erkner.py
+++ erkner.py
@@ -21,7 +21,9 @@ if not os.path.exists(folder):
 
 print("File we are working on : " + filename) 
 
-f2 = open(folder + "frog-tags_for_" + filename + ".txt", "w+") 
+frg = open(folder + "frog-tags_for_" + filename + ".txt", "w+") 
+
+fm = open(folder + "training-tags_for_" + filename + ".txt", "w+")
 
 toksentlist = []
 
@@ -33,6 +35,9 @@ alltokens = []
 
 frogtl = [] # tl => taglist
 
+trainingtl = []
+
+training = False
 
 firstline = True
 
@@ -47,14 +52,41 @@ for line in f:
 	
 	elif(len(dperl) > 2):	
 
-		#print(dperl[1] + "\t:\t" + dperl[9])
-		f2.write(dperl[1] + "," + dperl[9] + "\n")
+		token = dperl[1]
+
+		if(len(dperl) == 12):
+
+			training = True			
+
+			tag = dperl[9]
+
+			ttag = dperl[2]
+
+			if(ttag == "_"):
+				ttag = "O"
+
+			trainingtl.append(ttag)
+
+			#print(token + "," + ttag + "\n")
+			fm.write(token + "," + ttag + "\n")
+
+		elif(len(dperl) == 10):
+
+			tag = dperl[7]
+
+			fm.write("There was no training tag.\n")
+
+		else:
+			print("Input file format is not compatible")
+
+
+		frg.write(token + "," + tag + "\n")
 		
-		tokenlist.append(dperl[1])	
+		tokenlist.append(token)	
 
-		alltokens.append(dperl[1])
+		alltokens.append(token)
 
-		frogtl.append(dperl[9])	
+		frogtl.append(tag)	
 
 
 	else:
@@ -96,24 +128,38 @@ polytl = erkpoly.ner(texttotal, filename, folder)
 print("Polyglot done.")
 
 
-totallist = zip(alltokens, frogtl, nltktl, polytl)
-
-
 f3 = open(folder + "results_for" + filename + ".txt", "w+")
 
-f3.write("Token , Frog Tag, NLTK Tag, Polyglot Tag\n")
-f3.write("========================================\n")
 
-for to, fro, nlt, pol in totallist:
 
-	print(to  + "," + fro + "," + nlt + "," + pol)
-	f3.write(to  + "," + fro + "," + nlt + "," + pol + "\n")
+if(training == False):
+
+	totallist = zip(alltokens, frogtl, nltktl, polytl)
+
+	f3.write("Token , Frog Tag, NLTK Tag, Polyglot Tag\n")
+	f3.write("========================================\n")
+
+	for to, fro, nlt, pol in totallist:
+
+		print(to  + "," + fro + "," + nlt + "," + pol)
+		f3.write(to  + "," + fro + "," + nlt + "," + pol + "\n")
+
+elif(training == True):
 	
-	
+	totallist = zip(alltokens, trainingtl, frogtl, nltktl, polytl)
+
+	f3.write("Token, Training Tags, Frog Tag, NLTK Tag, Polyglot Tag\n")
+	f3.write("========================================\n")
+
+	for to, tra, fro, nlt, pol in totallist:
+
+		print(to + "," + tra + "," + fro + "," + nlt + "," + pol)
+		f3.write(to + "," + tra + "," + fro + "," + nlt + "," + pol + "\n")
 
 
 
 f.close()
-f2.close()
+frg.close()
+fm.close()
 f3.close()
 
